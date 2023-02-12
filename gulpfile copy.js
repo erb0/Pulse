6398fac98d151ff9c3c1babcee18738cc8a9907a
Erbol <erb0_01@mail.ru>
2023-02-12 11:00:43 +0600

    for github pages

--- gulpfile copy.js
+++ gulpfile copy.js
@@ -8,26 +8,26 @@ const rename = require("gulp-rename");
 gulp.task("server", function () {
   browserSync({
     server: {
-      baseDir: "docs",
+      baseDir: "src",
     },
   });
 
-  gulp.watch("docs/*.html").on("change", browserSync.reload);
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task("styles", function () {
   return gulp
-    .src("docs/sass/**/*.+(scss|sass)")
+    .src("src/sass/**/*.+(scss|sass)")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(rename({ suffix: ".min", prefix: "" }))
     .pipe(autoprefixer())
     .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(gulp.dest("docs/css"))
+    .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function () {
-  gulp.watch("docs/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
+  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
 });
 
 gulp.task("default", gulp.parallel("watch", "server", "styles"));
